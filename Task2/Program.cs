729880ba4837aef70fef2345c2450d299e150a3b
Yura <aderevs@gmail.com>
2024-02-12 15:37:09 +0200

    wirking version of task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        static ResourceMonitor rm = new ResourceMonitor(1_000_000);
+        static void BackgroundMemoryChecker()
+        {
+            while (true)
+            {
+                rm.CheckMemory();
+                Thread.Sleep(1000);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            new Thread(BackgroundMemoryChecker).Start();
+
+            int i = 0;
+            while (true)
+            {
+                decimal[] someMoney = new decimal[1_000];
+                Console.WriteLine($"Created sum number {i++}");
+                Thread.Sleep(75);
+            }
         }
     }
 }
---
+++ Task2/ResourceMonitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class ResourceMonitor
+    {
+        long maxMemory;
+
+        public ResourceMonitor(long maxMemory)
+        {
+            this.maxMemory = maxMemory;
+        }
+        public void CheckMemory()
+        {
+            long currentMemoryUsage = GC.GetTotalMemory(false);
+            if (currentMemoryUsage >= maxMemory)
+            {
+                Console.WriteLine($"Warning! Current memory usage: {currentMemoryUsage}, it's on the verge of established limit: {maxMemory}");
+            }
+        }
+    }
+}
